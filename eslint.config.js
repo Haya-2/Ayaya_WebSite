93f0e5ffd3ace1f04a0f9653ad68011b8d6c3530
Haya-2 <asirot.pro@gmail.com>
2025-09-29 23:08:51 +0200

    Ajout de règle contre les js variables sur le nav (ESLint autorise la lecture mais interdit l’écrasement)

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,19 @@ import js from "@eslint/js";
 export default [
    js.configs.recommended,
    {
+      languageOptions: {
+         globals: {
+            // navigateurs / DOM
+            window: "readonly",
+            document: "readonly",
+            console: "readonly",
+            fetch: "readonly",
+         },
+         parserOptions: {
+            ecmaVersion: 2021,
+            sourceType: "module",
+         },
+      },
       rules: {
          "no-unused-vars": "warn", // avertit si variable non utilisée
          "no-console": "off", // autorise console.log (pratique en dev)
